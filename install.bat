e8a1eb421f13ee6ffd5783b9830984dc4d84fbcf
Mr.programmer2938 <78996423+mrprogrammer2938@users.noreply.github.com>
2021-09-08 18:13:26 +0430

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,15 +3,9 @@ cls
 title Black-Tool Installing
 echo 
 echo Installing...
+echo Installing Ruby
 echo 
-start powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser && Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') && scoop install git
+chocho install ruby
 echo 
-echo installing ruby
-start chrome https://www.ruby-lang.org/en/downloads/
-echo 
-echo Installing Swift
-start chrome https://swift.org/download/
-echo 
-echo Finish...!
-echo
+echo Installing Finish
 PAUSE
